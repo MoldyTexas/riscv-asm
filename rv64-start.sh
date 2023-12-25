00e5e88043f24a8ef00221e4fe57034d17c9d814
Priyam Chaudhuri <chaudhuripriyam@yahoo.com>
2023-12-25 17:45:07 +0530

    workflow-edit

--- rv64-start.sh
+++ rv64-start.sh
@@ -4,5 +4,5 @@
 
 echo "Running QEMU-RV64 server"
 
-qemu-system-riscv64 -machine virt -nographic -m 2048 -smp 4 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.bin -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=eth0 -netdev user,id=eth0 -device virtio-rng-pci -drive file=ubuntu-22.04.3-preinstalled-server-riscv64+unmatched.img,format=raw,if=virtio
+qemu-system-riscv64 -machine virt -nographic -m 2048 -smp 4 -bios /usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.bin -kernel /usr/lib/u-boot/qemu-riscv64_smode/uboot.elf -device virtio-net-device,netdev=eth0 -netdev user,id=eth0 -device virtio-rng-pci -drive file=/home/moldytexas/riscv64-linux/qemu-riscv-setup/ubuntu-22.04.3-preinstalled-server-riscv64+unmatched.img,format=raw,if=virtio
 
--- workflow
+++ workflow
@@ -5,10 +5,17 @@
 			a. riscv64-linux-gnu
 			b. qemu-user
 			c. qemu-user-static
-() push code to git-repo 
+(DONE) push code to git-repo 
 (DONE) Run code in native RV64 machine on QEMU
 
 
-Create Emulator
-Implement ISA, new or existing
-Create Assembler, if req
+(NOT NEEDED) 	Create Emulator
+				Implement ISA, new or existing
+				Create Assembler, if req
+
+Write program on rv-asm to run natively on rv-64 machine
+Potential kinds of programs:
+	1. Convertor
+	2. Calculator
+	3. 
+
